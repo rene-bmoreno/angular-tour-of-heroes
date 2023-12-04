1aa1b263aaea43c59015eb1e6a579af127470157
Rene Bulnes <renebulnes2@gmail.com>
2023-12-03 19:44:32 -0500

    Deleted in memory data service files and updated the app module and hero service files. Changed the private heroUrl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from './in-memory-data.service';
+
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -20,14 +19,7 @@ import { MessagesComponent } from './messages/messages.component';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    HttpClientModule,
-
-    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
-    // and returns simulated server responses.
-    // Remove it when a real server is ready to receive requests.
-    HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryDataService, { dataEncapsulation: false }
-    )
+   
   ],
   declarations: [
     AppComponent,
